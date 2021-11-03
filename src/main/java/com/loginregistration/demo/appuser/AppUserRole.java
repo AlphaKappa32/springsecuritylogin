76d51203001ec6d5ec77af9e6ce12addae0d11c5
AlphaKappa32 <aggeloskar32@gmail.com>
2021-11-03 23:35:24 +0200

    Initial commit

---
+++ src/main/java/com/loginregistration/demo/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.loginregistration.demo.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/loginregistration/demo/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,29 @@
+package com.loginregistration.demo.registration.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.tomcat.jni.Local;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    //save the token
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepository.save(token);
+    }
+
+    public Optional<ConfirmationToken> getToken(String token){
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    public int setConfirmedAt(String token){
+        return confirmationTokenRepository.updateConfirmedAt(token, LocalDateTime.now());
+    }
+}
